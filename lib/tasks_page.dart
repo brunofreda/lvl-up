b666ebdb91f1bf8c26c2dfcfa2bd62f982b0b9cf
Bruno Freda <brunodemianfreda@gmail.com>
2023-07-23 17:42:29 -0300

    Made a popup for the adding of the task

--- lib/tasks_page.dart
+++ lib/tasks_page.dart
@@ -48,32 +48,10 @@ class _TasksPageState extends State<TasksPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             // TODO: Set the color of the text relating it with the theme
-            // TODO: Abstract the text, form and tasks from the scaffold
+            // TODO: Abstract the text and tasks from the scaffold
             const Text(
               'Tap the plus button to add a task',
               style: TextStyle(color: Colors.grey),
-            ),
-            Form(
-              key: formKey,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Flexible(
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        // TODO: Make the color of the hint text grey.
-                        hintText: 'New task',
-                      ),
-                      onSaved: (newTask) => _addTask(newTask),
-                    ),
-                  ),
-                  Flexible(
-                    child: IconButton(
-                        onPressed:() => 0, icon: const Icon(Icons.access_time)
-                    ),
-                  ),
-                ],
-              ),
             ),
             Text(
               '$_tasks',
@@ -84,9 +62,54 @@ class _TasksPageState extends State<TasksPage> {
       // TODO: Abstract button from scaffold
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          if (formKey.currentState?.validate() == true) {
-            formKey.currentState?.save();
-          }
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return SizedBox(
+                height: 200,
+                child: Center(
+                  // TODO: Abstract form from scaffold
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            // TODO: Make the color of the hint text grey.
+                            hintText: 'New task',
+                          ),
+                          onSaved: (newTask) => _addTask(newTask),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Flexible(
+                              child: ElevatedButton(
+                                child: const Text('Save'),
+                                onPressed: () {
+                                  if (formKey.currentState?.validate() == true) {
+                                    formKey.currentState?.save();
+                                  }
+
+                                  Navigator.pop(context);
+                                },
+                              )
+                            ),
+                            Flexible(
+                              child: IconButton(
+                                  onPressed:() => 0,
+                                  icon: const Icon(Icons.access_time)
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }
+          );
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
